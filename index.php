10f88f9883a0c8df7a4ce29dfe259bc8a45cc38d
AntonovOS <mclewis1337@gmail.com>
2026-02-07 18:07:22 +0500

    new file:   css/font1.css 	new file:   css/font2.css 	deleted:    font.css 	modified:   index.php 	new file:   profile.php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
     require_once('linkBD\dbconnect.php');
+
+    if (!empty($_POST['login']) && !empty($_POST['password'])) {
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+        if ($login == 'admin' && $password == '12345') 
+        {
+            setcookie('login', $login, time()+3600, "/"); //Создание куки
+            header('Location: /profile.php'); //Переход на другую страницу
+            die();
+        }
+        else 
+        {
+            header('Location: /index.php'); //Переход на другую страницу
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -7,30 +22,31 @@
 <head>
     <meta charset="UTF-8" />
     <title>Login</title>
-    <link rel="stylesheet" href="font.css">
+    <link rel="stylesheet" href="css/font1.css">
 </head>
+<body>
 <header> 
     <p class = "UpperText">Введите логин и пароль для входа в WEB приложение</p>
 </header>
-<body>
-        <div class = register> Окно для ввода
-            <form class = register action="main" id = "add-form">
-                <fieldset>
-                    <legend>Данные</legend>
-                        <p><label>Логин </label><input type="login" placeholder="Введите логин"></p>
-                        <p><label>Пароль </label><input type="password" placeholder="Введите пароль"></p>
-                </fieldset>
-
-                <div class = "submit">
-                    <button type = "submit" id = "submit">Отправить</button>
-                </div>
-                
-            </form>
+  
+<form method="POST" action="index.php">
+    <fieldset>
+        <div class = CenterText>
+            <label class="login">Логин:</label> <input type="login" id = "login" name = "login" placeholder="Введите логин">
+        </div>
+        
+        <div class = CenterText>
+            <label class = "password">Пароль: </label> <input type="password" id = "password" name = "password" placeholder="Введите пароль">
         </div>
 
+        <div>
+            <button type="submit">Войти</button>
+        </div>
 
-</body>
+    </fieldset>
+</form>
 <footer>
 
 </footer>
+</body>
 </html>
---
+++ profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>profile</title>
+    <link rel="stylesheet" href="font2.css">
+</head>
+<header> 
+    
+</header>
+<body>
+   Привет     
+
+
+</body>
+<footer>
+
+</footer>
+</html>
